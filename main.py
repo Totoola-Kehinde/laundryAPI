94f0328f8ec7b93cc09b1c693478afe78ad41b73
Totoola Kenny <40864358+Totoola-Kehinde@users.noreply.github.com>
2020-09-17 02:12:20 +0100

    implementing login endpoint

--- main.py
+++ main.py
@@ -24,6 +24,26 @@ def create_user(email, username, password):
     newuser.password = password
     return dict(newuser)
 
+def email_exists(email):
+    user_exist = True
+    if connection.db.users.find(
+        {'email': email}
+    ).count() == 0:
+        user_exist = False
+        return user_exist
+
+def check_login_creds(email, password):
+    if email_exists(email):
+        activeuser = connection.db.users.find(
+            {'email': email}
+        )
+        activeuser = dict(activeuser)
+        print(activeuser)
+        return activeuser
+    else:
+        return "Invalid Email"
+
+
 app = FastAPI()
 
 
@@ -52,3 +72,20 @@ def signup(email, username: str, password: str):
     elif user_exists == False:
         connection.db.users.insert_one(data)
         return {"message":"User Created","email": data['email'], "name": data['name'], "pass": data['password']}
+
+# Login endpoint
+@app.get("/login/{email}/{password}")
+def login(email, password):
+    def log_user_in(creds):
+        if creds['email'] == email and creds['password'] == password:
+            return {"message": creds['name'] + 'successfully logged in'}
+        else:
+            return {"message":"Invalid credentials!!"}
+    # Read email from database to validate if user exists and checks if password matches
+    logger = check_login_creds(email, password)
+    print(logger)
+    if bool(logger) == True:
+        return {"message":logger}
+    else:
+        status = log_user_in(logger)
+        return {"Info":status}
